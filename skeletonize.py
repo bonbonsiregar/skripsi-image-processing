341a41b9fd606801702680dfbafde57c32eb1d07
Baginda Bonar Siregar <bagindov@outlook.com>
2020-03-29 18:51:24 +0700

    newest revision

--- skeletonize.py
+++ skeletonize.py
@@ -1,11 +1,14 @@
 import cv2
 import numpy as np
- 
+
+# Load image yang terakhir telah dilakukan Morphological Opening 
 img = cv2.imread('0_1.jpg',0)
+# Buat matrix dengan nama size dan menyimpan jumlah elemen yang terdapat di img
 size = np.size(img)
+# Buat matrix dengan dimensi yang sama dengan img, tetapi diisi dengan nilai nol = hitam
 skel = np.zeros(img.shape,np.uint8)
- 
-ret,img = cv2.threshold(img,127,255,0)
+
+# buat kernel dengan bentuk cross 3x3
 element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
 done = False
  
@@ -20,5 +23,6 @@ while(done is False):
     if zeros==size:
         done = True
 cv2.imshow("skel",skel)
+cv2.imwrite('0_2.jpg',skel)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- skripsi.py
+++ skripsi.py
@@ -1,22 +1,19 @@
 import cv2
-import glob
-import os
 import numpy as np
-import imutils
-from skimage import data
-from skimage.data import imread
 import matplotlib.pyplot as plt
-from skimage.morphology import skeletonize, thin
 
-image = cv2.imread("2.jpg")
-img = cv2.imread("2.jpg")
-#image = cv2.imread("2.jpg")
-#img = cv2.imread("2.jpg")
-kernel_closing = np.ones((20,20),np.uint8)
-kernel_opening = np.ones((5,5),np.uint8)
+# Load Image
+image = cv2.imread("C:\\Users\\USER\\Documents\\skripsi_image_processing\\gambar meloidogyne\\Meloidogyne chitwoodi\melochit20_BP-01_NebraskaSupermarket-edit.jpg")
+# Load Image untuk threshold
+img = cv2.imread("C:\\Users\\USER\\Documents\\skripsi_image_processing\\gambar meloidogyne\\Meloidogyne chitwoodi\melochit20_BP-01_NebraskaSupermarket-edit.jpg")
+# Matrix untuk Morphological closing
+kernel_closing = np.ones((15,15),np.uint8)
+# Matrix untik Morphological opening
+kernel_opening = np.ones((3,3),np.uint8)
+# konversi gambar berwarna 3 color channel ke grayscale dengan 1 color channel
 gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+# gaussian blur dengan kernel matrix 3x3
 gaussian_blur = cv2.GaussianBlur(gray_img,(3, 3),0)
-done = False
 
 ret, threshold_img = cv2.threshold(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY),127,255,cv2.THRESH_BINARY)
 
@@ -30,71 +27,39 @@ def auto_canny(image, sigma=0.33):
  
 
     return edged
-
+# Canny manual dengan jarak value yang lebar
 wide = cv2.Canny(gaussian_blur,10,200)
+# Canny manual dengan jarak value yang kecil
 tight = cv2.Canny(gaussian_blur,225,250)
+# Auto Canny Edge Detection
 auto = auto_canny(gaussian_blur)
 
-#opening = cv2.morphologyEx(auto,cv2.MORPH_OPEN,kernel_opening)
-closing = cv2.morphologyEx(auto, cv2.MORPH_CLOSE,kernel_closing)
+# Melakukan morphological closing untuk mengisi bagian dalam objek 
+closing = cv2.morphologyEx(wide, cv2.MORPH_CLOSE,kernel_closing)
+
+# Melakukan morphological opening untuk menghapus noise di luar objek
 opening = cv2.morphologyEx(closing,cv2.MORPH_OPEN,kernel_opening)
-#closing2 = cv2.morphologyEx(closing, cv2.MORPH_CLOSE,kernel)
-#ret, threshold_img = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
-img2 = opening
-size = np.size(img2)
-skel = np.zeros(img2.shape,np.uint8)
-element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
-
-#skeleton = skeletonize(auto)
-#thinned = thin(threshold_img)
-#thinned_partial = thin(threshold_img,max_iter=25)
 
 
-contours, hier = cv2.findContours(opening, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#cv2.drawContours(img, contours, -1, (255,255,0),1)
+cv2.imwrite('0_1.jpg',opening)
 
-for c in contours:
+caption = ['image', 'grayscale', 'gaussian blur', 'canny','threshold', 'closing', 'opening']
+images = [image,gray_img,gaussian_blur,auto,threshold_img,closing,opening]
 
-    x, y, w, h = cv2.boundingRect(c)
-    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
-
-    rect = cv2.minAreaRect(c)
-    box = cv2.boxPoints(rect)
-
-    box = np.int0(box)
-
-    cv2.drawContours(img,[box],0,(0,0,255))
-
-    (x,y), radius = cv2.minEnclosingCircle(c)
-    center = (int(x), int(y))
-    radius = int(radius)
-
-    img = cv2.circle(img,center,radius,(255,0,0),2)
-
-while(not done):
-    eroded = cv2.erode(img2,element)
-    temp = cv2.dilate(img2,element)
-    temp = cv2.subtract(img2,temp)
-    skel = cv2.bitwise_or(skel,temp)
-    img2 = eroded.copy()
-
-    zeros = size - cv2.countNonZero(img2)
-    if zeros == size:
-        done = True 
-
-#print jumlah contours yang terdeteksi
-print(len(contours))
-
-cv2.drawContours(img, contours, -1, (255,255,0),1)
-cv2.imwrite('0_1.jpg',closing)
-
-caption = ['image', 'grayscale', 'gaussian blur', 'canny', 'contours', 'threshold', 'opening', 'closing']
-images = [image,gray_img,gaussian_blur,auto,img,threshold_img,opening,closing]
-
-for i in range(8):
-    plt.subplot(3,3, i+1), plt.imshow(images[i], 'gray')
-    plt.title(caption[i])
-    plt.xticks([]),plt.yticks([])
+for i in range(7):
+     plt.subplot(3,3, i+1), plt.imshow(images[i], 'gray')
+     plt.title(caption[i])
+     plt.xticks([]),plt.yticks([])
     
 plt.show()
+
+#cv2.imshow('image', image);
+#cv2.imshow('grayscale', gray_img);
+#cv2.imshow('gaussian blur', gaussian_blur);
+cv2.imshow('canny', wide);
+#cv2.imshow('threshold', threshold_img);
+#cv2.imshow('closing', closing);
+cv2.imshow('opening', opening);
 cv2.waitKey(0)
 cv2.destroyAllWindows()
